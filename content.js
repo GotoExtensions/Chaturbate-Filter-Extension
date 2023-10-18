6229b67911e54f4d32c97ca702da0aa9eccbf263
CBFilter <148009553+CBFilter@users.noreply.github.com>
2023-10-18 19:09:15 +0000

    Add files via upload

--- content.js
+++ content.js
@@ -1,71 +1,32 @@
-const injectScript = `var chRoot = document.querySelector(".msg-list-fvm.message-list");
-
-let FilteredNotice = [];
-let FilteredTip = [];
-let noticeOn = false;
-let tipOn = false;
-
-function callback(mutationList, observer) {
-  let newNodes = mutationList[0].addedNodes;
-  for (const node of newNodes) {
-    let filtered;
-    let trgtArray;
-    if ((filtered = node.querySelector(".roomNotice"))) {
-      if (filtered.classList.contains("isTip")) {
-        FilteredTip.push(filtered);
-        if (tipOn) filtered.parentElement.style.display = "none";
-      } else {
-        let cnt = 0;
-        let notFound = true;
-        for (const elm of FilteredNotice) {
-          if (filtered.isEqualNode(elm)) {
-            FilteredNotice[cnt].parentElement.remove();
-            FilteredNotice[cnt] = filtered;
-            if (noticeOn) filtered.parentElement.style.display = "none";
-            notFound = false;
-            break;
-          }
-          cnt++;
-        }
-        if (notFound) FilteredNotice.push(filtered);
-        if (noticeOn) filtered.parentElement.style.display = "none";
-      }
-    }
-  }
+function getScript(attachment) {
+  fetch(browser.runtime.getURL("headerscript.js"))
+    .then((response) => response.text())
+    .then((scriptContent) => {
+      let scriptTag = document.createElement("script");
+      scriptTag.textContent = attachment + scriptContent;
+      document.head.appendChild(scriptTag);
+    })
+    .catch((error) => {
+      console.error("Failed to load script:", error);
+    });
 }
-
-const observer = new MutationObserver(callback);
-observer.observe(chRoot, { childList: true });
-
-function toggleTips() {
-  if (tipOn) {
-    for (const elm of FilteredTip) elm.parentElement.style.display = "block";
-  } else {
-    for (const elm of FilteredTip) elm.parentElement.style.display = "none";
-  }
-  tipOn = !tipOn;
-}
-
-function toggleNotice() {
-  if (noticeOn) {
-    for (const elm of FilteredNotice) elm.parentElement.style.display = "block";
-  } else {
-    for (const elm of FilteredNotice) elm.parentElement.style.display = "none";
-  }
-  noticeOn = !noticeOn;
-}`;
 
 const url = window.location.href;
-console.log("urlhere:" + url);
 const reg = /\S*testbed.cb.dev\/b\//;
 let css1;
+const cross = browser.runtime.getURL("resource/cross-svgrepo-com.svg");
+const tickURL = browser.runtime.getURL("resource/tick-svgrepo-com.svg");
+let urls = `const tick = "${tickURL}";
+const cross = "${cross}";
+ `;
+
 if (reg.test(url)) {
   const elm = document.querySelector(
     ".navigationAlt2BgImage.navigationAlt2BgColor.tabSectionBorder"
   );
   elm.insertAdjacentHTML(
     "beforeend",
-    "<span class='spacer'></span><span class='addonbtn' onclick='toggleTips()'>tips</span><span class='addonbtn' onclick='toggleNotice()'>room notice</span>"
+    `<span class='spacer'></span><span class='addonbtn' onclick='toggleTips()'><img src='${cross}' id='extTipsSvg' alt='off' class="extBtnSvg"/>tips</span><span class='addonbtn' onclick='toggleNotice()'><img src='${cross}' id='extNoteSvg'  class="extBtnSvg" />room notice</span>`
   );
   css1 = `.spacer{
             margin-left:auto;
@@ -74,7 +35,7 @@ if (reg.test(url)) {
   const elm = document.querySelector("[ts='c']");
   elm.insertAdjacentHTML(
     "afterbegin",
-    "<span class='addonbtn' onclick='toggleTips()'>tips</span><span class='addonbtn' onclick='toggleNotice()'>room notice</span>"
+    `<span class='addonbtn' onclick='toggleTips()'><img src=${cross} id='extTipsSvg'  class="extBtnSvg" />tips</span><span class='addonbtn' onclick='toggleNotice()'><img src='${cross}' id='extNoteSvg' class="extBtnSvg" />room notice</span>`
   );
   elm.style.width = "auto";
 }
@@ -83,9 +44,11 @@ const style = document.createElement("style");
 style.innerHTML = `
     .addonbtn {
         border: 1px solid rgb(139, 179, 218);
+        display: inline-flex;
+        align-items:center;
         border-radius: 4px;
-        height: 1.6em;
-        padding: 0.05em 0.8em;
+        
+        padding: 0.05em 0.8em 0em;
         margin: auto 0.5em auto;
         width: auto;
         overflow: visible;
@@ -93,10 +56,15 @@ style.innerHTML = `
         font-family: UbuntuMedium, Helvetica, Arial, sans-serif;
         font-size: 1.1rem;
     }
-    ${css1}
+    .addonbtn + .addonbtn {
+      margin-right: 1.5em;
+    }    
+    .extBtnSvg {
+      margin-top:0.15em;
+    }
+
+    ${css1}  
     `;
 
 var addedNode = document.head.appendChild(style);
-var scriptNode = document.createElement("script");
-scriptNode.innerHTML = injectScript;
-document.head.appendChild(scriptNode);
+getScript(urls);
